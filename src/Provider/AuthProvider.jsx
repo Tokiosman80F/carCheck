359d4a5aa679231c1ac6ffe10b4cd9432d31e11d
Tokiosman80F <tokiosman96@gmail.com>
2023-11-15 23:31:40 +0600

    signout using firebase implemented

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -4,6 +4,7 @@ import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
@@ -12,12 +13,17 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loader, setLoader] = useState(true);
+
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
   const loginUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+  const logoutUser=()=>{
+    return signOut(auth)
+  }
   useEffect(() => {
     const unsubcribe = () => {
       onAuthStateChanged(auth, (currentUser) => {
@@ -31,7 +37,7 @@ const AuthProvider = ({ children }) => {
     };
   }, []);
 
-  const authInfo = { user,loader,createUser, loginUser };
+  const authInfo = { user,loader,createUser, loginUser,logoutUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
