586cb971c47c99f56f9a9033bbabd3d457a9d62b
Tokiosman80F <tokiosman96@gmail.com>
2023-11-15 11:33:29 +0600

    register page complete

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,20 +1,32 @@
+import { Link } from "react-router-dom";
 import logImage from "../../assets/login/login.svg";
-import google from "../../assets/login/google.svg";
 
 const LoginPage = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const userInfo = { email, password };
+    console.log(userInfo);
+  };
   return (
     <div>
-      <div className="hero h-screen">
-        <div className="hero-content flex-col lg:flex-row gap-48">
-          <div>
+      <div className="md:hero my-10 md:my-0  md:h-screen gap-3">
+        <div className="md:hero-content flex  ">
+          <div className="hidden md:block">
             <img src={logImage} alt="" />
           </div>
-          <div className=" card flex-shrink-0 w-full max-w-sm  ">
-            <form className="card-body text-dark2 border   border-primaryColor rounded-2xl">
-            <h1 className="text-center  text-3xl font-semibold ">Login </h1>
+          <div className=" card flex-shrink-0 w-full max-w-sm ">
+            <form
+              onSubmit={handleLogin}
+              className="card-body text-dark2 border-2  rounded-2xl"
+            >
+              <h1 className="text-center  text-3xl font-semibold ">Login </h1>
               <div className="form-control">
                 <label className="label">
-                  <span >Email</span>
+                  <span>Email</span>
                 </label>
                 <input
                   type="email"
@@ -26,7 +38,7 @@ const LoginPage = () => {
               </div>
               <div className="form-control">
                 <label className="label">
-                  <span >Password</span>
+                  <span>Password</span>
                 </label>
                 <input
                   type="password"
@@ -34,16 +46,25 @@ const LoginPage = () => {
                   className="input input-bordered"
                   required
                   name="password"
-                /> 
+                />
               </div>
-              <div className="form-control mt-6">
-                <button className="btn capitalize text-white bg-primaryColor hover:bg-black">Login with Email</button>
-              </div>
+
+              <div className="flex items-center gap-2 mt-2">
+                <input
+                  type="submit"
+                  value="Login with Email"
+                  className="btn capitalize text-white bg-primaryColor hover:bg-black"
+                />
+
                 <p className="text-center"> or</p>
-                <div className="form-control mt-6">
-                   
-                <button className="btn btn-outline capitalize">  <img src={google} alt="" /> Login with google</button>
+
+                <input
+                  type="submit"
+                  value="Login with google"
+                  className="btn btn-outline capitalize"
+                />
               </div>
+              <p>New Here? <Link to="/register" className="text-primaryColor underline">Sign Up </Link></p>
             </form>
           </div>
         </div>
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,93 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import logImage from "../../assets/login/login.svg";
 
 const RegisterPage = () => {
-    return (
-        <div>
-            Registration
+    const handleRegister = (e) => {
+        e.preventDefault();
+    
+        const form = e.target;
+        const name=form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const userInfo = { name,email, password };
+        console.log(userInfo);
+      };
+  return (
+    <div>
+      <div className="md:hero my-10 md:my-0  md:h-screen gap-3">
+        <div className="md:hero-content flex  ">
+          <div className="hidden md:block">
+            <img src={logImage} alt="" />
+          </div>
+          <div className=" card flex-shrink-0 w-full max-w-sm ">
+            <form
+              onSubmit={handleRegister}
+              className="card-body text-dark2 border-2  rounded-2xl"
+            >
+              <h1 className="text-center  text-3xl font-semibold ">Register </h1>
+              <div className="form-control">
+                <label className="label">
+                  <span>Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                  required
+                  name="name"
+                />
+              </div>
+             
+              <div className="form-control">
+                <label className="label">
+                  <span>Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                  name="email"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span>Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                  name="password"
+                />
+              </div>
+
+              <div className="flex flex-col  gap-2 mt-2">
+                <input
+                  type="submit"
+                  value="SignUp with Email"
+                  className="btn capitalize text-white bg-primaryColor hover:bg-black"
+                />
+
+                <p className="text-center"> or</p>
+
+                <input
+                  type="submit"
+                  value="SignUp with google"
+                  className="btn btn-outline capitalize"
+                />
+              </div>
+              <p>
+                Already have an account?
+                <Link className="text-primaryColor underline">Login </Link>
+              </p>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
